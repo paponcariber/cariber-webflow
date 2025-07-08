6273429a0ab7796dd65c4215e6641edf6ab98e81
unknown <papon@cariber.co>
2025-07-08 11:00:32 +0800

    add parameter workshop

--- cariber_utm.js
+++ cariber_utm.js
@@ -1,21 +1,29 @@
 $(document).ready(function () {
   var URLSearchParams_wb = new URLSearchParams(window.location.search);
   var utmParameters = ["utm_source", "utm_medium", "utm_campaign"];
+
   for (let i = 0; i < utmParameters.length; i++) {
     var utm_element = utmParameters[i];
-    /* if utm_source exist */
     $("form").each(function (index) {
       if (URLSearchParams_wb.has(utm_element)) {
-        /* get UTM value of this utm param */
         var value = URLSearchParams_wb.get(utm_element);
-        /* change form hidden field to this utm url value */
         $(this)
           .find("." + utm_element)
           .val(value);
       }
     });
-  } /* end for loop */
-  $(this)
-    .find("." + "TimeStamp")
-    .val(new Date().toLocaleString("sv", { timeZone: "Asia/Bangkok" }));
+  }
+
+  if (URLSearchParams_wb.has("workshop")) {
+    var workshopValue = URLSearchParams_wb.get("workshop");
+    $("form").each(function () {
+      $(this).find('input[name="course"]').val(workshopValue);
+    });
+  }
+
+  $("form").each(function () {
+    $(this)
+      .find(".TimeStamp")
+      .val(new Date().toLocaleString("sv", { timeZone: "Asia/Bangkok" }));
+  });
 });
